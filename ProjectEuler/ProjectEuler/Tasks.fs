5b8c72aff527b824cdc098a4091ad4ec3524b129
Jacques <viniciusjacques@gmail.com>
2015-06-03 14:53:26 -0400

    tasks

---
+++ ProjectEuler/ProjectEuler/Tasks.fs
@@ -0,0 +1,59 @@
+﻿module Tasks
+
+open System
+open System.Threading.Tasks
+
+[<EntryPoint>]
+let main argv = 
+    let work = 
+        async { 
+            for i in 0..2 do
+                printfn "Work loop is currently %O" i
+                do! Async.Sleep(1000)
+        }
+    
+    let rec sum x y = 
+        match x with
+        | 0 -> y
+        | _ -> 
+            let total = x + y
+            sum (x - 1) total
+    
+    let sumTask = 
+        async { 
+            do! Async.Sleep(5000)
+            return sum 1000 0
+        }
+    
+    let asyncWorkflow = 
+        async { 
+            let task = Async.StartAsTask(work)
+            let! result = Async.AwaitTask(task)
+            printfn "task1 completed"
+        }
+    
+    let asyncWorkflow2 = 
+        async { 
+            let task2 = Async.StartAsTask(sumTask)
+            let! result2 = Async.AwaitTask(task2)
+            printfn "task2  completed"
+            return result2
+        }
+    
+    let job5 = 
+        async { 
+            let tcs = TaskCompletionSource<int>()
+            Async.Start(async { 
+                            do! Async.Sleep 1000
+                            tcs.SetResult (sum 1000 0)
+                        })
+            return! (Async.AwaitTask tcs.Task)
+        }
+    
+    Async.Start asyncWorkflow
+    let finalResult2 = Async.RunSynchronously asyncWorkflow2
+    printfn "Task result is: %O" finalResult2
+    let finalResult3 = Async.RunSynchronously job5
+    printfn "tcs result is: %O" finalResult3
+    Console.ReadLine() |> ignore
+    0
---
+++ ProjectEuler/Tasks/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Task task1 = new Task(new Action(printMessage));
+            Task task2 = new Task(delegate { printMessage(); });
+            Task task3 = new Task(() => printMessage());
+            Task task4 = new Task(() => { printMessage(); });
+
+            task1.Start();
+            task2.Start();
+            task3.Start();
+            task4.Start();
+
+            Task<Int32> t = new Task<Int32>(n => Sum((Int32)n), 1000);
+            t.Start();
+
+            Task ctw = t.ContinueWith(task => Console.WriteLine("The sum is: "
+                + task.Result));
+
+            Console.WriteLine("Main method complete. Press <enter> to finish.");
+            Console.ReadLine();
+        }
+
+        private static void printMessage()
+        {
+            Console.WriteLine("Hello, world!");
+        }
+
+        private static Int32 Sum(Int32 n)
+        {
+            Int32 sum = 0;
+            for (; n > 0; n--)
+                checked { sum += n; }
+
+            return sum;
+        }
+    }
+}
